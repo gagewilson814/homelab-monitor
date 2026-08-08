76813ce0c1998e68ab4b3e91f344486d7280e5b2
gagewilson814 <gage.wilson814@gmail.com>
2026-08-08 14:52:12 -0600

    implement cpu and mem usage

--- server.go
+++ server.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/mem"
 )
 
 type Response struct {
@@ -14,18 +18,49 @@ type Response struct {
 	Uptime      string  `json:"uptime"`
 }
 
+func getHostname() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Println("Error getting hostname: ", err)
+		return "unknown"
+	}
+	return hostname
+}
+
+func getMemoryUsage() float64 {
+	memory, err := mem.VirtualMemory()
+	if err != nil {
+		log.Println("Error getting memory usage: ", err)
+		return 0.0
+	}
+	return memory.UsedPercent
+}
+
+func getCPUUsage() float64 {
+	cpuUsage, err := cpu.Percent(0, false)
+	if err != nil {
+		log.Println("Error getting CPU usage: ", err)
+		return 0.0
+	}
+	return cpuUsage[0]
+
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	response := Response{
-		Hostname:    "localhost", // Placeholder for actual hostname
-		CPUUsage:    0.0,         // Placeholder for actual CPU usage
-		MemoryUsage: 0.0,         // Placeholder for actual memory usage
-		DiskUsage:   0.0,         // Placeholder for actual disk usage
-		Uptime:      "0s",        // Placeholder for actual uptime
+		Hostname:    getHostname(),    // Placeholder for actual hostname
+		CPUUsage:    getCPUUsage(),    // Placeholder for actual CPU usage
+		MemoryUsage: getMemoryUsage(), // Placeholder for actual memory usage
+		DiskUsage:   0.0,              // Placeholder for actual disk usage
+		Uptime:      "0s",             // Placeholder for actual uptime
 	}
 	log.Println("Sending response:")
 	log.Println(response)
-	log.Fatal(json.NewEncoder(w).Encode(response))
+	jsonResponse := json.NewEncoder(w).Encode(response)
+	if jsonResponse != nil {
+		log.Println("Error encoding JSON response: ", jsonResponse)
+	}
 }
 
 func main() {
